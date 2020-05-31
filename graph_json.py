13a2bbe71db3d406363d4ad9f985f371fdbc1905
aidatorajiro <kawarusosu@zoho.com>
2020-05-31 13:35:10 +0900

    implement graph_json

--- graph_json.py
+++ graph_json.py
@@ -6,9 +6,10 @@ import shelve
 from scipy.sparse import lil_matrix, csr_matrix, load_npz, save_npz
 import math
 from scipy.spatial import cKDTree
+import json
 
 if __name__ == "__main__":
-    nodeid_to_pos = np.load(os.path.dirname(os.path.abspath(__file__)) + '/graph_pre_nodeid_to_pos.npz')['arr_0']
-    graph_csr = load_npz(os.path.dirname(os.path.abspath(__file__)) + '/graph_pre_graph_csr.npz')
-    graph_lil = lil_matrix(graph_csr)
+    nodeid_to_pos = np.load(os.path.dirname(os.path.abspath(__file__)) + '/graph_post_nodeid_to_pos.npz')['arr_0']
+    with open("graph_post_nodeid_to_pos.json", "w") as f:
+        json.dump(nodeid_to_pos.tolist(), f)
     
